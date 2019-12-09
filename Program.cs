1565da4f7c52e260c910bf9f0595df4477446625
Anh Truong <xdah031@yahoo.com>
2019-12-09 17:17:33 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OclExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "42,12";
+            List<int> list = new List<int>() { 4, 4, 5, 61, 1, 4, 1, 1, 1 };
+            List<int> list2 = new List<int>() { 1, 4, 5 };
+            Console.WriteLine("Hello Extension Methods!");
+            Console.WriteLine(list.IncludesAll(list2));
+            Console.WriteLine(list.NotEmpty());
+            Console.WriteLine(s.ToReal());
+        }
+    }
+}
